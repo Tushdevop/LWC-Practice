3dd43a02ed8fc15d04f5e20abfbf18adbc619d1a
Tushdevop <120645045+Tushdevop@users.noreply.github.com>
2023-05-05 18:14:44 +0530

    05mayeve

--- LearnLWC/force-app/main/default/lwc/lwcReceipeApexMethods/lwcReceipeApexMethods.js
+++ LearnLWC/force-app/main/default/lwc/lwcReceipeApexMethods/lwcReceipeApexMethods.js
@@ -1,6 +1,12 @@
 import { LightningElement, wire } from 'lwc';
+import { getSObjectValue } from '@salesforce/apex';
+
 import getContactList from '@salesforce/apex/LWCRecipeApexController.getContactList';
 import getMultipleContacts from '@salesforce/apex/LWCRecipeApexController.getMultipleContacts';
+import getSingleContact from '@salesforce/apex/LWCRecipeApexController.getSingleContact';
+
+import NAME_FIELD from '@salesforce/schema/Contact.Name';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
 
 export default class LwcReceipeApexMethods extends LightningElement {
 
@@ -11,6 +17,7 @@ errors;
 @wire(getContactList) contacts;
 
 
+// Wired function
 @wire(getMultipleContacts) 
 contactLists({ error, data}){
     if(data){
@@ -21,4 +28,16 @@ contactLists({ error, data}){
         this.conList = undefined;
     }
 }
+
+//getter method to get name 
+@wire(getSingleContact) contact;
+get name() {
+    return this.contact.data ? getSObjectValue(this.contact.data, NAME_FIELD) : '';
+}
+get email() {
+    return this.contact.data ? getSObjectValue(this.contact.data, EMAIL_FIELD) : '';
+}
+
+//method
+
 }
