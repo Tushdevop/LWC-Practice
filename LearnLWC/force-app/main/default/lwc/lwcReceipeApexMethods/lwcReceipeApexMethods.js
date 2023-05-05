a7132f070946e536bfb19839770f1596f07af3f5
Tushdevop <120645045+Tushdevop@users.noreply.github.com>
2023-05-05 20:36:27 +0530

    apexmethods

--- LearnLWC/force-app/main/default/lwc/lwcReceipeApexMethods/lwcReceipeApexMethods.js
+++ LearnLWC/force-app/main/default/lwc/lwcReceipeApexMethods/lwcReceipeApexMethods.js
@@ -4,14 +4,23 @@ import { getSObjectValue } from '@salesforce/apex';
 import getContactList from '@salesforce/apex/LWCRecipeApexController.getContactList';
 import getMultipleContacts from '@salesforce/apex/LWCRecipeApexController.getMultipleContacts';
 import getSingleContact from '@salesforce/apex/LWCRecipeApexController.getSingleContact';
+import getAccountList from '@salesforce/apex/LWCRecipeApexController.getAccountList';
 
 import NAME_FIELD from '@salesforce/schema/Contact.Name';
 import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+
+/** The delay used when debouncing event handlers before invoking Apex. */
+const DELAY = 300;
 
 export default class LwcReceipeApexMethods extends LightningElement {
 
 conList;
 errors;
+
+Bcontacts;
+Berror;
+
+searchKey='';
 
 //wired property
 @wire(getContactList) contacts;
@@ -30,6 +39,7 @@ contactLists({ error, data}){
 }
 
 //getter method to get name 
+//Call Apex method and get SObject values using a static schema definition.
 @wire(getSingleContact) contact;
 get name() {
     return this.contact.data ? getSObjectValue(this.contact.data, NAME_FIELD) : '';
@@ -38,6 +48,29 @@ get email() {
     return this.contact.data ? getSObjectValue(this.contact.data, EMAIL_FIELD) : '';
 }
 
-//method
+
+//method Retrieve a list of records using an imperative Apex method call.
+handleLoad(){
+    getContactList()
+    .then((result) =>{
+        this.Bcontacts = result;
+        this.Berror = undefined;
+    })
+    .catch((error) => {
+        this.Berror = error;
+        this.Bcontacts = undefined;
+    })
+}
+
+//Call an Apex method with parameters using @wire
+@wire(getAccountList, { searchString : '$searchKey' }) Accounts;
+
+handleChange(event){
+    window.clearTimeout(this.delayTimeout);
+    const searchKey = event.target.value;
+    this.delayTimeout = setTimeout(() => {
+        this.searchKey = searchKey;
+    }, DELAY);
+}   
 
 }
