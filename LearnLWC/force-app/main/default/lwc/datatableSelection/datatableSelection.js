ca898640082e9c95c68c91e2075d9dcbaaacc36d
Tushdevop <120645045+Tushdevop@users.noreply.github.com>
2023-05-09 18:15:47 +0530

    09may

---
+++ LearnLWC/force-app/main/default/lwc/datatableSelection/datatableSelection.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+import getAccountListforDel from '@salesforce/apex/AccountDataMethods.getAccountListforDel';
+
+import { deleteRecord } from 'lightning/uiRecordApi';
+import { refreshApex } from '@salesforce/apex';
+
+
+export default class DatatableSelection extends LightningElement {
+
+
+}
