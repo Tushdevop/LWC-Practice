84281d7237587585bdb3fd38319c8f29afda50b1
Tushdevop <120645045+Tushdevop@users.noreply.github.com>
2023-06-05 19:00:44 +0530

    05Junevechanges

--- LearnLWC/force-app/main/default/lwc/dataCommunicationChildComp/dataCommunicationChildComp.js
+++ LearnLWC/force-app/main/default/lwc/dataCommunicationChildComp/dataCommunicationChildComp.js
@@ -1,3 +1,13 @@
 import { LightningElement } from 'lwc';
 
-export default class DataCommunicationChildComp extends LightningElement {}
+export default class DataCommunicationChildComp extends LightningElement {
+
+    closeHandler(){
+        const childEvent = new CustomEvent('close',  { bubbles:true, detail: "Modal closed successfully"})
+        this.dispatchEvent(childEvent)
+    }
+
+    footerHandler(){
+        console.log('Footer is called');
+    }
+}
--- LearnLWC/force-app/main/default/lwc/dataCommunicationParentComp/dataCommunicationParentComp.js
+++ LearnLWC/force-app/main/default/lwc/dataCommunicationParentComp/dataCommunicationParentComp.js
@@ -1,3 +1,14 @@
 import { LightningElement } from 'lwc';
 
-export default class DataCommunicationParentComp extends LightningElement {}
+export default class DataCommunicationParentComp extends LightningElement {
+
+    showModal = false;
+    message
+    clickHandler(){
+        this.showModal = true;
+    }
+    parrentCloseHandler(event){
+        this.message = event.detail
+        this.showModal = false;
+    }
+}
